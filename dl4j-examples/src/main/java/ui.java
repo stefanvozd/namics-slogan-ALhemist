635f195d18bb3eaa6a243246542cffc4e840f2b5
91stefan@gmail.com <91stefan@gmail.com>
2017-04-04 19:17:46 +0200

    gui hello world

---
+++ dl4j-examples/src/main/java/ui.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Stefan on 04.04.2017..
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ui extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
